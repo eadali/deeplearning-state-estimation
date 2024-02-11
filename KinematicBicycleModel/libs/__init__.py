8a02be63581680d5b46d8c47d79c6ac1b8cc1003
lacivert <erkanadali91@gmail.com>
2024-02-11 13:07:52 +0200

    Kinematic bicycle model is added

---
+++ KinematicBicycleModel/libs/__init__.py
@@ -0,0 +1,4 @@
+from libs.car_description import CarDescription
+from libs.normalise_angle import normalise_angle
+from libs.cubic_spline_interpolator import generate_cubic_spline
+from libs.stanley_controller import StanleyController
